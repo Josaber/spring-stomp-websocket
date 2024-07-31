d3199883909c8a4817b6687c834c3496d590c6a9
Josaber <1174980997@qq.com>
2024-07-31 14:59:12 +0800

    refactor: update ide warning

--- src/main/java/org/spring/messaging/websocket/stomp/TriggerController.java
+++ src/main/java/org/spring/messaging/websocket/stomp/TriggerController.java
@@ -8,7 +8,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("ws/trigger")
+@RestController("ws")
 public class TriggerController {
     private static final Logger log = LoggerFactory.getLogger(TriggerController.class);
 
@@ -21,7 +21,7 @@ public class TriggerController {
         this.webSocketProperties = webSocketProperties;
     }
 
-    @PostMapping("message")
+    @PostMapping("trigger/message")
     @Operation(summary = "Trigger Hello Message"/*, security = @SecurityRequirement(name = "bearerAuth")*/)
     public Greeting sendFiles() {
         log.info("WebSocket message triggered");
