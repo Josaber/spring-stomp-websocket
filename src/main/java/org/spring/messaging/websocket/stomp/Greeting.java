3b294e13c8c0b164322dfa29315b658c324d66f9
Josaber <1174980997@qq.com>
2024-07-31 14:59:11 +0800

    feat: initialize by spring example

---
+++ src/main/java/org/spring/messaging/websocket/stomp/Greeting.java
@@ -0,0 +1,18 @@
+package org.spring.messaging.websocket.stomp;
+
+public class Greeting {
+
+	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
